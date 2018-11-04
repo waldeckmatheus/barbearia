6bd202224ddde7f6ae318f033e520bd4a19d048e
Waldeck Matheus <waldeck@prodam.am.gov.br>
2018-11-03 23:28:51 -0400

    Início do layout da listagem de usuários

--- src/app/usuario/usuario-list/usuario-list.component.ts
+++ src/app/usuario/usuario-list/usuario-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-usuario-list',
   templateUrl: './usuario-list.component.html',
-  styleUrls: ['./usuario-list.component.scss']
+  styleUrls: ['./usuario-list.component.scss'],
+  animations: [routerTransition()]
 })
 export class UsuarioListComponent implements OnInit {
-
+  usuarioList = [
+    {login:'usuario01',nome:'Usuário 01', telefone:'987654321'},
+    {login:'usuario02',nome:'Usuário 02', telefone:'997654321'}
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 import { UsuarioListComponent } from './usuario-list/usuario-list.component';
 import { UsuarioRoutingModule } from './usuario-routing.module';
+import { PageHeaderModule } from '../shared';
 
 @NgModule({
   declarations: [UsuarioFormComponent, UsuarioListComponent],
   imports: [
-    CommonModule, UsuarioRoutingModule
+    CommonModule, UsuarioRoutingModule, PageHeaderModule
   ]
 })
 export class UsuarioModule { }
